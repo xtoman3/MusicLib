382c1230469e12e484d9043b51ad345970d08877
Daniel Košč <daniel.kosc@gmail.com>
2023-05-08 14:06:11 +0200

    Sorting options added

--- src/pages/Artists.tsx
+++ src/pages/Artists.tsx
@@ -1,5 +1,5 @@
-import { Box } from '@mui/material';
-import React, { FC, useEffect } from 'react';
+import { Box, MenuItem, Select } from '@mui/material';
+import React, { FC, useEffect, useState } from 'react';
 
 import usePageTitle from '../hooks/usePageTitle';
 import { useSpotifyApi } from '../hooks/useSpotifyApi';
@@ -27,33 +27,67 @@ const Artists: FC = () => {
 			.catch(error => alert(error));
 	}, [savedArtistIds]);
 
+	enum SortOptions {
+		Followers = 'Followers',
+		Popularity = 'Popularity',
+		Name = 'Name'
+	}
+
+	const [sortOption, setSortOption] = useState<SortOptions>(
+		SortOptions.Followers
+	);
+
+	const getSortFunc = (a: ArtistPreviewType, b: ArtistPreviewType) => {
+		switch (sortOption) {
+			case SortOptions.Followers:
+				return b.followers.total - a.followers.total;
+			case SortOptions.Popularity:
+				return (b.popularity ?? 0) - (a.popularity ?? 0);
+			case SortOptions.Name:
+				return a.name.localeCompare(b.name);
+		}
+	};
+
 	return (
-		<Box
-			sx={{
-				display: 'flex',
-				flexDirection: 'row',
-				flexWrap: 'wrap',
-				justifyContent: 'center',
-				alignItems: 'flex-start',
-				alignContent: 'flex-start'
-			}}
-		>
-			{artists
-				?.filter(artist => savedArtistIds.has(artist.id))
-				.sort(
-					(a: ArtistPreviewType, b: ArtistPreviewType) =>
-						b.followers.total - a.followers.total
-				)
-				.map(artist => (
-					<ArtistPreview
-						key={artist.id}
-						artist={artist}
-						rating={ratings.get(artist.id) ?? 0}
-						saved={savedArtistIds.has(artist.id)}
-						showRating
-					/>
-				))}
-		</Box>
+		<>
+			<Select
+				labelId="select-label"
+				id="select"
+				variant="standard"
+				value={sortOption}
+				onChange={e =>
+					setSortOption(SortOptions[e.target.value as keyof typeof SortOptions])
+				}
+				sx={{ marginLeft: 2 }}
+			>
+				<MenuItem value={SortOptions.Followers}>Followers</MenuItem>
+				<MenuItem value={SortOptions.Popularity}>Popularity</MenuItem>
+				<MenuItem value={SortOptions.Name}>Name</MenuItem>
+			</Select>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'row',
+					flexWrap: 'wrap',
+					justifyContent: 'center',
+					alignItems: 'flex-start',
+					alignContent: 'flex-start'
+				}}
+			>
+				{artists
+					?.filter(artist => savedArtistIds.has(artist.id))
+					.sort(getSortFunc)
+					.map(artist => (
+						<ArtistPreview
+							key={artist.id}
+							artist={artist}
+							rating={ratings.get(artist.id) ?? 0}
+							saved={savedArtistIds.has(artist.id)}
+							showRating
+						/>
+					))}
+			</Box>
+		</>
 	);
 };
 
