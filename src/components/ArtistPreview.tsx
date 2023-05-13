b559b27622c9ad339c842daea6abac4dc46bfaf7
Samuel Toman <485618@muni.cz>
2023-05-13 15:37:40 +0200

    ArtistPreview now uses Grid

--- src/components/ArtistPreview.tsx
+++ src/components/ArtistPreview.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, Paper, Typography } from '@mui/material';
+import { Box, Grid, IconButton, Paper, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import ClearIcon from '@mui/icons-material/Clear';
 import React, { FC } from 'react';
@@ -38,102 +38,104 @@ const ArtistPreview: FC<Props> = ({ artist, saved, rating, showRating }) => {
 	};
 
 	return (
-		<Paper
-			key={artist.id}
-			sx={{
-				p: 2,
-				m: 1,
-				maxWidth: 250,
-				display: 'flex',
-				flexDirection: 'column'
-			}}
-		>
-			<Box
+		<Grid item key={artist.id} xs={12} sm={6} md={4} lg={3}>
+			<Paper
+				key={artist.id}
 				sx={{
+					p: 2,
+					m: 1,
+					maxWidth: 250,
 					display: 'flex',
-					flexDirection: 'row'
+					flexDirection: 'column'
 				}}
 			>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'row'
+					}}
+				>
+					<Typography
+						variant="h6"
+						title={artist.name}
+						// @ts-ignore
+						onClick={() => navigate({ to: `/artist/${artist.id}` })}
+						sx={{
+							'overflow': 'hidden',
+							'whiteSpace': 'nowrap',
+							'textOverflow': 'ellipsis',
+							'&:hover': {
+								cursor: 'pointer',
+								color: 'primary.main',
+								opacity: 1
+							}
+						}}
+					>
+						{artist.name}
+					</Typography>
+					<Box sx={{ flexGrow: 1 }} />
+					<IconButton
+						onClick={handleSubmit}
+						size="small"
+						sx={{
+							'marginLeft': 1,
+							'backgroundColor': 'primary.main',
+							'opacity': 1,
+							'boxShadow': '0px 2px 4px rgba(0, 0, 0, 0.25)',
+							'&:hover': {
+								cursor: 'pointer',
+								backgroundColor: 'primary.dark',
+								opacity: 1
+							}
+						}}
+					>
+						{saved ? <ClearIcon /> : <AddIcon />}
+					</IconButton>
+				</Box>
+
 				<Typography
-					variant="h6"
-					title={artist.name}
-					// @ts-ignore
-					onClick={() => navigate({ to: `/artist/${artist.id}` })}
+					variant="subtitle1"
+					title="Number of followers"
 					sx={{
-						'overflow': 'hidden',
-						'whiteSpace': 'nowrap',
-						'textOverflow': 'ellipsis',
-						'&:hover': {
-							cursor: 'pointer',
-							color: 'primary.main',
-							opacity: 1
-						}
+						overflow: 'hidden',
+						whiteSpace: 'pre-wrap',
+						display: 'block'
 					}}
 				>
-					{artist.name}
+					followers: {artist.followers.total}
 				</Typography>
-				<Box sx={{ flexGrow: 1 }} />
-				<IconButton
-					onClick={handleSubmit}
-					size="small"
+
+				<Typography
+					variant="subtitle1"
+					title={artist.genres[0]}
 					sx={{
-						'marginLeft': 1,
-						'backgroundColor': 'primary.main',
-						'opacity': 1,
-						'boxShadow': '0px 2px 4px rgba(0, 0, 0, 0.25)',
-						'&:hover': {
-							cursor: 'pointer',
-							backgroundColor: 'primary.dark',
-							opacity: 1
-						}
+						overflow: 'hidden',
+						whiteSpace: 'pre-wrap',
+						display: 'block'
 					}}
 				>
-					{saved ? <ClearIcon /> : <AddIcon />}
-				</IconButton>
-			</Box>
-
-			<Typography
-				variant="subtitle1"
-				title="Number of followers"
-				sx={{
-					overflow: 'hidden',
-					whiteSpace: 'pre-wrap',
-					display: 'block'
-				}}
-			>
-				followers: {artist.followers.total}
-			</Typography>
-
-			<Typography
-				variant="subtitle1"
-				title={artist.genres[0]}
-				sx={{
-					overflow: 'hidden',
-					whiteSpace: 'pre-wrap',
-					display: 'block'
-				}}
-			>
-				{getFormattedGenres(artist.genres)}
-			</Typography>
-			<img
-				src={artist.images[0]?.url ?? 'https://shorturl.at/cnqtP'}
-				alt={artist.name}
-			/>
-			<Typography
-				variant="subtitle2"
-				title="popularity"
-				sx={{
-					overflow: 'hidden',
-					whiteSpace: 'pre-wrap',
-					display: 'block'
-				}}
-			>
-				popularity: {artist.popularity}
-			</Typography>
-			{showRating && (
-				<RatingStrip id={artist.id} type="Artist" initStars={rating ?? 0} />
-			)}
-		</Paper>
+					{getFormattedGenres(artist.genres)}
+				</Typography>
+				<img
+					src={artist.images[0]?.url ?? 'https://shorturl.at/cnqtP'}
+					alt={artist.name}
+				/>
+				<Typography
+					variant="subtitle2"
+					title="popularity"
+					sx={{
+						overflow: 'hidden',
+						whiteSpace: 'pre-wrap',
+						display: 'block'
+					}}
+				>
+					popularity: {artist.popularity}
+				</Typography>
+				{showRating && (
+					<RatingStrip id={artist.id} type="Artist" initStars={rating ?? 0} />
+				)}
+			</Paper>
+		</Grid>
 	);
 };
 
